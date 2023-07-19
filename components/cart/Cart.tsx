d35079bce767683c200a8bae7a204412dd21d0c7
riddhasoftchris <88536680+riddhasoftchris@users.noreply.github.com>
2023-07-19 22:53:37 +0545

    cart wip

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -18,11 +18,11 @@ export default function Cart({ open, setOpen }: any) {
                     leaveFrom="opacity-100"
                     leaveTo="opacity-0"
                 >
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    <div className="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
                 </Transition.Child>
                 <div className="fixed inset-0 overflow-hidden">
                     <div className="absolute inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                        <div className="fixed inset-y-0 right-0 flex max-w-full pl-10 pointer-events-none">
                             <Transition.Child
                                 as={Fragment}
                                 enter="transform transition ease-in-out duration-500 sm:duration-700"
@@ -32,13 +32,13 @@ export default function Cart({ open, setOpen }: any) {
                                 leaveFrom="translate-x-0"
                                 leaveTo="translate-x-full"
                             >
-                                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                                    <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
-                                        <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                                <Dialog.Panel className="w-screen max-w-md pointer-events-auto">
+                                    <div className="flex flex-col h-full overflow-y-scroll bg-white shadow-xl">
+                                        <div className="flex-1 px-4 py-6 overflow-y-auto sm:px-6">
                                             <CartHeader setOpen={setOpen} />
                                             <CartItems />
                                         </div>
-                                        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+                                        <div className="px-4 py-6 border-t border-gray-200 sm:px-6">
                                             <div className="flex justify-between text-base font-medium text-gray-900">
                                                 <p>Subtotal</p>
                                                 <p>Rs {totalAmount}</p>
@@ -47,7 +47,7 @@ export default function Cart({ open, setOpen }: any) {
                                             <div className="mt-6">
                                                 <a
                                                     href="#"
-                                                    className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                                                    className="flex items-center justify-center px-6 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700"
                                                 >
                                                     Checkout
                                                 </a>
@@ -67,14 +67,14 @@ export default function Cart({ open, setOpen }: any) {
 const CartHeader = ({ setOpen }: any) => {
     return <div className="flex items-start justify-between">
         <Dialog.Title className="text-lg font-medium text-gray-900">Shopping cart</Dialog.Title>
-        <div className="ml-3 flex h-7 items-center">
+        <div className="flex items-center ml-3 h-7">
             <button
                 type="button"
-                className="-m-2 p-2 text-gray-400 hover:text-gray-500"
+                className="p-2 -m-2 text-gray-400 hover:text-gray-500"
                 onClick={() => setOpen(false)}
             >
                 <span className="sr-only">Close panel</span>
-                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                <XMarkIcon className="w-6 h-6" aria-hidden="true" />
             </button>
         </div>
     </div>
@@ -89,24 +89,24 @@ const CartItems = () => {
             <ul role="list" className="-my-6 divide-y divide-gray-200">
                 {cartItems.map((product: any, index: number) => (
                     <li key={product?.id} className="flex py-6">
-                        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <div className="flex-shrink-0 w-24 h-24 overflow-hidden border border-gray-200 rounded-md">
                             <img
                                 src={product?.image}
                                 alt={product?.imageAlt}
-                                className="h-full w-full object-contain object-center"
+                                className="object-contain object-center w-full h-full"
                             />
                         </div>
-                        <div className="ml-4 flex flex-1 flex-col">
+                        <div className="flex flex-col flex-1 ml-4">
                             <div>
                                 <div className="flex justify-between text-base font-medium text-gray-900">
                                     <h3>
-                                        <a href={product?.name}>{product?.name}</a>
+                                        <a href={product?.title}>{product?.title}</a>
                                     </h3>
                                     <p className="ml-4">{product?.price}</p>
                                 </div>
                                 <p className="mt-1 text-sm text-gray-500">{product?.color}</p>
                             </div>
-                            <div className="flex flex-1 items-end justify-between text-sm">
+                            <div className="flex items-end justify-between flex-1 text-sm">
                                 <p className="text-gray-500">Qty {product?.quantity}</p>
                                 <div className="flex">
                                     <button
